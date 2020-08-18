7a337b21212ba694d2257f0d6714c61711cec247
souta <s.k_stbsk.r12@docomo.ne.jp>
2020-08-18 19:58:46 +0000

    rails04課題

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     validates :name, presence: true
     validates :email, presence: true
+    validates :password, length: { minimum: 8, maximum: 32 }
     
     has_secure_password
     
