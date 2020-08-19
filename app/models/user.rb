8e2576f7d4d0be0a8650319dd9acc7c66cff0dca
souta <s.k_stbsk.r12@docomo.ne.jp>
2020-08-19 15:06:17 +0000

    rails04課題完成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
-    validates :name, presence: true
-    validates :email, presence: true
-    validates :password, length: { minimum: 8, maximum: 32 }
+    validates :name, presence: true,length: { maximum: 15 }
+    VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/
+    validates :email, presence: true,format:{ with:VALID_EMAIL_REGEX }
+    VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]{8,32}+\z/
+    validates :password, length: { minimum: 8, maximum: 32 },format:{ with: VALID_PASSWORD_REGEX }
     
     has_secure_password
     
