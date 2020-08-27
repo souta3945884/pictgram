4acaa89a71e51aaec48b1a6e5e79c7415f3e83c4
souta <s.k_stbsk.r12@docomo.ne.jp>
2020-08-27 03:09:59 +0000

    rails07テキスト

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -10,15 +10,15 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   def extension_whitelist
-  %w(jpg jpeg png)
+    %w(jpg jpeg png)
   end
   
   def size_range
-   1..10.megabytes
+    1..10.megabytes
   end
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
