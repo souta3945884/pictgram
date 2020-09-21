07823b3dbddbbe4a385e88d340b200ad946869aa
souta <s.k_stbsk.r12@docomo.ne.jp>
2020-09-21 11:53:51 +0000

    いいねを解除できないエラーを修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -16,7 +16,7 @@ class FavoritesController < ApplicationController
   end
   
   def destroy
-    Favorite.find_by(params[:topic_id]).destroy
+    Favorite.find_by(topic_id: params[:topic_id]).destroy
     flash[:success] = "お気に入りを解除しました"
     redirect_to topics_path
   end
