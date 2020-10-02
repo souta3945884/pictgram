6bfe76805edc5f3468c660bddb19b161a4f78e50
souta <s.k_stbsk.r12@docomo.ne.jp>
2020-10-02 16:53:13 +0000

    修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,15 +3,21 @@ class CommentsController < ApplicationController
     @comment_topics = current_user.favorite_topics
   end
   
+  def new
+    @comment = Comment.new
+    @comment.user_id = current_user.id
+    @comment.topic_id = params[:topic_id]
+  end
+  
   def create
-    comment = Comment.new
-    comment.user_id = current_user.id
-    comment.topic_id = params[:topic_id]
+    @comment = Comment.new
+    @comment.user_id = current_user.id
+    @comment.topic_id = params[:topic_id]
     
-    if comment.save
+    if @comment.save
       redirect_to topics_path, success: 'コメントを投稿しました'
     else
-      redirect_to topics_path, danger: 'コメントの投稿に失敗しました'
+      render action: :new
     end
   end
 end
